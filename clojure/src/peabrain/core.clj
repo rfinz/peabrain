c41b189dd6aa1e9b7b17871cd71f215f84af9570
Raymond Finzel <finze006@umn.edu>
2019-06-26 16:37:23 -0500

    was reading a style guide... changed some style

--- clojure/src/peabrain/core.clj
+++ clojure/src/peabrain/core.clj
@@ -3,38 +3,34 @@
             [peabrain.util :as pb]))
 
 (defn compute-guess
-  "Compute guess (mat) for LAYERS using F as the activation function.
-LAYERS should look like [W3 W2 W1 INPUTS]"
+  "Compute guess (mat) for `layers` using `f` as the activation function.
+  `layers` should look like [W3 W2 W1 INPUTS]"
   [layers f]
   (if (= 1 (count layers))
     (first layers)
     (pb/map-mat f (nc/mm (pb/right-ones (compute-guess (rest layers) f)) (first layers)))))
 
 (defn back-prop
-  "Produce matrix of slopes for list of matrices LAYERS and expected VALUES.
-  Derivative of activation function F-PRIME and activation function F. LAYERS should look like [W3 W2 W1 INPUTS]"
+  "Produce matrix of slopes for list of matrices `layers` and expected `values`.
+  Derivative of activation function `f-prime` and activation function `f`. `layers` should look like [W3 W2 W1 INPUTS]"
   ([layers values f f-prime] (back-prop layers values f f-prime nil))
   ([layers values f f-prime previous]
    (if (< 1 (count layers))
-     (let
-         [x
-          (if (not previous)
-            ;; first time entering the function, do this:
-            (pb/hadamard
-             (pb/map-mat - values (compute-guess layers f))
-             (nc/scal! -1.0 (pb/map-mat f-prime (nc/mm (pb/right-ones (compute-guess (rest layers) f)) (first layers))))
-             )
-            ;; if value from a previous calculation is available do this:
-            (pb/hadamard
-             (nc/mm
-              (if (= 1 (get previous :loopnum)) (get previous :derivative) (pb/right-shrink (get previous :derivative)))
-              (nc/trans (get previous :weights))
-              )
-             (pb/right-ones (pb/map-mat f-prime (nc/mm (pb/right-ones (compute-guess (rest layers) f)) (first layers))))
-             )   
-            )]
+     (let [x (if-not previous
+               ;; first time entering the function, do this:
+               (pb/hadamard
+                (pb/map-mat - values (compute-guess layers f))
+                (nc/scal! -1.0 (pb/map-mat f-prime (nc/mm (pb/right-ones (compute-guess (rest layers) f)) (first layers)))))
+               ;; if value from a previous calculation is available do this:
+               (pb/hadamard
+                (nc/mm
+                 (if (= 1 (:loopnum previous)) (:derivative previous) (pb/right-shrink (:derivative previous)))
+                 (nc/trans (:weights previous)))
+                (pb/right-ones (pb/map-mat f-prime (nc/mm (pb/right-ones (compute-guess (rest layers) f)) (first layers))))))]
        (cons
-        (nc/mm (pb/bottom-ones (nc/trans (compute-guess (rest layers) f))) (if previous (pb/right-shrink x) x)) ; insert gradient
+        (nc/mm
+         (pb/bottom-ones (nc/trans (compute-guess (rest layers) f)))
+         (if previous (pb/right-shrink x) x)) ; insert gradient
         (back-prop
          (rest layers)
          values
@@ -43,11 +39,4 @@ LAYERS should look like [W3 W2 W1 INPUTS]"
          {
           :derivative x
           :weights (first layers)
-          :loopnum (if previous (+ 1 (get previous :loopnum)) 1)
-          }
-         )
-        )
-       )
-     )
-   )
-  )
+          :loopnum (if previous (+ 1 (:loopnum previous)) 1)}))))))
--- clojure/src/peabrain/util.clj
+++ clojure/src/peabrain/util.clj
@@ -6,19 +6,19 @@
             [clojure.data.csv :as csv]))
 
 (defn hadamard
-  "Entrywise product of M1 M2."
+  "Entrywise product of `m1` and `m2`."
   [m1 m2]
   (nn/dge (map (fn [r1 r2] (map * r1 r2)) (nc/rows m1) (nc/rows m2)))
   )
 
 (defn random-matrix
-  "Initialize random matrix of height R and width C."
+  "Initialize random matrix of height `r` and width `c`."
   [r c]
   (nn/dge r c (repeatedly (* r c) rand))
   )
 
 (defn shuffle
-  "Shuffle the rows of matrix M. Shuffling is done in place."
+  "Shuffle the rows of matrix `m`. Shuffling is done in place."
   [m]
   (doseq [i (reverse (range 1 (nc/mrows m)))]
     (na/swap-rows! m (nn/iv (rand-int i) i))
@@ -26,36 +26,37 @@
   )
 
 (defn right-ones
-  "Add a 1 to the end of every row in matrix M."
+  "Add a 1 to the end of every row in matrix `m`."
   ([m]
    (nn/dge (vec (map (fn [r] (conj (vec (map (fn [i] i) r)) 1.0)) (nc/rows m))))
    )
   )
 
 (defn right-shrink
-  "Remove a column from the right hand side of matrix M."
+  "Remove a column from the right hand side of matrix `m`."
   [m]
   (nc/submatrix m (nc/mrows m) (- (nc/ncols m) 1))
   )
 
 (defn bottom-ones
-  "Add a row of 1s to the bottom of matrix M."
+  "Add a row of 1s to the bottom of matrix `m`."
   [m]
-  ; forcing intermediate result to vec allows conj to append to end
+  ;; forcing intermediate result to vec allows conj to append to end
   (nn/dge (conj (vec (map (fn [r] (map (fn [i] i) r)) (nc/rows m))) (repeat (nc/ncols m) 1.0)))
   )
 
 (defn bottom-vals
-  "Add a row of V to the bottom of matrix M."
+  "Add a row of `v` to the bottom of matrix `m`."
   [m v]
   (nn/dge (conj (vec (map (fn [r] (map (fn [i] i) r)) (nc/rows m))) (repeat (nc/ncols m) v)))
   )
 
 (defn map-mat
-  "Apply function F to every item in matrix M."
+  "Apply function `f` to every item in matrix `m`."
   ([f m]
    (nn/dge (map (fn [r] (map f r)) (nc/rows m))))
-  ([f m1 m2] ;; this could maybe be adapted to work with an arbitrary number of matrices
+  ([f m1 m2]
+   ;; this could maybe be adapted to work with an arbitrary number of matrices
    (nn/dge (map (fn [r1 r2] (map f r1 r2)) (nc/rows m1) (nc/rows m2))))
   )
 
@@ -64,5 +65,5 @@
   [reader & options]
   (nn/dge
    (vec (map
-     (fn [r] (map #(Double/parseDouble %) r)) (rest (apply csv/read-csv reader options)))))
+     (fn [r] (map Double/parseDouble r)) (rest (apply csv/read-csv reader options)))))
   )
